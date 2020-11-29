fb3f07f00c2aa16229083432c5d32d271ece5c20
Contiinuum <68207985+Contiinuum@users.noreply.github.com>
2020-11-29 13:46:29 +0100

    init

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using AudicaModding;
+
+[assembly: AssemblyTitle(CommandManager.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(CommandManager.BuildInfo.Company)]
+[assembly: AssemblyProduct(CommandManager.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + CommandManager.BuildInfo.Author)]
+[assembly: AssemblyTrademark(CommandManager.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(CommandManager.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(CommandManager.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(CommandManager), CommandManager.BuildInfo.Name, CommandManager.BuildInfo.Version, CommandManager.BuildInfo.Author, CommandManager.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
